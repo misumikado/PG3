6c935cf2887b412cf8e05f982e379b9035dec7e7
MSI\k021g <k021g1161@g.neec.ac.jp>
2022-12-06 14:50:47 +0900

    2_1

--- main.cpp
+++ main.cpp
@@ -4,59 +4,51 @@
 #include <iostream>
 using namespace std;
 
-void* malloc(size_t size);
-
-typedef struct cell {
-
-	int val;
-	struct cell* next;
-
-}CELL;
-
-void create(CELL* head, int x);
-void index(CELL* head);
-
 int main()
 {
-	int val;
-	CELL head;
-	head.next = nullptr;
 
-	while (true) {
-		printf("追加する値を入力:");
-		scanf_s("%d", &val);
+	printf("==1970==\n");
+	std::list<string>lst = {
+	"oosaki","gotannda","meguro","ebisu","sibuya","harazyuku",
+	"yoyogi","sinnzyuku","sinnookubo","takadanobaba","meziro",
+	"ikebukuro","ootuka","sugamo","komagome","tabata",
+	"nippori","uguisudani","ueno","okatimati","akihabara","kannda",
+	"toukyou","yuurakutyou","sinnbasi","hamamatutyou","tamata","sinagawa"
+	};
 
-		create(&head, val);
-
-		index(&head);
-
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+		std::cout << *itr << "\n";
 	}
 
+	printf("==2019==\n");
+
+	for (std::list<string>::iterator itr = lst.begin(); itr != lst.end(); ++itr) {	//西日暮里を追加
+		if (*itr == "nippori")
+		{
+			itr = lst.insert(itr, "nisinippori");
+			++itr;
+		}
+	}
+
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+		std::cout << *itr << "\n";
+	}
+
+	printf("==2022==\n");
+
+	for (std::list<string>::iterator itr = lst.begin(); itr != lst.end(); ++itr) {	//高野輪ゲートウェイを追加
+		if (*itr == "sinagawa")
+		{
+			itr = lst.insert(itr, "takanawage-toway");
+			++itr;
+		}
+	}
+
+	for (auto itr = lst.begin(); itr != lst.end(); ++itr) {
+		std::cout << *itr << "\n";
+	}
+
+	system("pause");
 	return 0;
 }
 
-
-void create(CELL* head, int x) {
-	CELL* cell;
-	//メモリを確保
-	cell = (CELL*)malloc(sizeof(CELL));
-
-	cell->val = x;
-	cell->next = nullptr;
-
-	while (head->next != nullptr) {
-		head = head->next;
-	}
-	head->next = cell;
-}
-
-void index(CELL* head) {
-
-	printf("[");
-	while (head->next != nullptr) {
-		head = head->next;	
-		printf("%d,", head->val);
-	}
-	printf("]\n");
-	printf("-------------------------------\n");
-}
