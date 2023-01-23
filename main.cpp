72f7917646f46f641735bc9a6839ba080b125a66
MSI\k021g <k021g1161@g.neec.ac.jp>
2023-01-24 08:55:20 +0900

    3_1

--- main.cpp
+++ main.cpp
@@ -1,61 +1,27 @@
-#include <stdio.h>
-#include <list>
-#include <string.h>
-
-using namespace std;
+#include "test.h"
 
 int main()
 {
-	list<const char*>stations =
-	{
-	 "Tokyo","Kanda","Akihabara",
-	 "Okachimachi","Ueno","Uguisudani",
-	 "Nippori","Tabata","Komagome",
-	 "Sugamo","Otsuka","Ikebukuro",
-	 "Mejiro","Takadanobaba","Shin-Okubo",
-	 "Shinjuku","Yoyogi","Harajuku",
-	 "Shibuya","Ebisu","Meguro",
-	 "Gotanda","Osaki","Shinagawa",
-	 "Tamachi","Hamamatsucho","Shimbashi",
-	 "Yurakucho"
-	};
+    SceneChange* sceneManager = SceneChange::GetInstance();
+    while (true)
+    {
+        sceneManager->PrintScene();
 
+    input:
+        int putNum = 0;
 
-	printf("---1970---\n");
-	for (auto itr = stations.begin(); itr != stations.end(); itr++)
-	{
-		printf("%s\n", *itr);
-	}
+        printf("0~3‚ð‰Ÿ‚µ‚Ä‚­‚¾‚³‚¢\n");
 
-	for (auto itr = stations.begin(); itr != stations.end(); itr++)
-	{
-		if (strcmp(*itr, "Tabata") == 0)
-		{
-			stations.insert(itr, "Nishi-Nippori");
-			break;
-		}
-	}
+        scanf_s("%d", &putNum);
 
-	printf("---2019---\n");
-	for (auto itr = stations.begin(); itr != stations.end(); itr++)
-	{
-		printf("%s\n", *itr);
-	}
-
-	for (auto itr = stations.begin(); itr != stations.end(); itr++)
-	{
-		if (strcmp(*itr, "Tamachi") == 0)
-		{
-			stations.insert(itr, "Takanawa-Geto-Way");
-			break;
-		}
-	}
-
-	printf("---2022---\n");
-	for (auto itr = stations.begin(); itr != stations.end(); itr++)
-	{
-		printf("%s\n", *itr);
-	}
-
-	return 0;
+        switch (putNum)
+        {
+        case 0:
+        case 1:
+        case 2:
+        case 3:
+            sceneManager->ChangeScene(putNum);
+            break;
+        }
+    }
 }
